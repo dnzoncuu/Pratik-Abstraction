52ab34e133c899fa81970092b1fa0bed7e4212ec
dnzoncuu <berkoncu17@gmail.com>
2025-05-25 11:52:19 +0300

    Add project files.

---
+++ Pratik - Abstraction/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik___Abstraction
+{
+    public abstract class Person
+    {
+        public string _ad {  get; set; }
+
+        public string _soyad {  get; set; }
+
+        public string _departman {  get; set; }
+
+        public abstract void Görev();
+        public void BilgileriYazdır()
+        {
+            Console.WriteLine($"Adım : {_ad}  ,  Soyadım : {_soyad}  , Departman : {_departman}");
+        }
+    }
+    public class Patron : Person
+    {
+        public override void Görev()
+        {
+            Console.WriteLine("Yapı Var !!!");
+        }
+    }
+    public class ProjeYonetimi : Person
+    {
+        public override void Görev()
+        {
+            Console.WriteLine("Benim Görevim Proje Yönetimi");
+        }
+    }
+    public class Yazilimci : Person
+    {
+        public override void Görev()
+        {
+            Console.WriteLine("Ben yazılımcıyım");
+        }
+    }
+}
---
+++ Pratik - Abstraction/Program.cs
@@ -0,0 +1,29 @@
+﻿using Pratik___Abstraction;
+
+Yazilimci yazilim = new Yazilimci
+{
+    _ad = "Deniz",
+    _soyad = "Öncü",
+    _departman = "Bilgi İşlem",
+    
+};
+yazilim.BilgileriYazdır();
+yazilim.Görev();
+Console.WriteLine("-------------------------------------------");
+ProjeYonetimi yönetici = new ProjeYonetimi
+{
+    _ad = "Sergen",
+    _soyad = "Yalçın",
+    _departman = "yönetici"
+};
+yönetici.BilgileriYazdır();
+yönetici.Görev();
+Console.WriteLine("-------------------------------------------");
+Patron  patron = new Patron
+{
+    _ad = "Ali",
+    _soyad = "Koç",
+    _departman = "İstifa etme bizi bırakma"
+};
+patron.BilgileriYazdır();
+patron.Görev();
